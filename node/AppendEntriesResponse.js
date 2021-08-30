bdfca6e1f259b65adc9cd5249992de8905904b7b
Ronald Chen <ronald@battlefy.com>
2021-08-30 16:24:09 -0700

    implemented calculateMajorityMatchIndex

--- node/AppendEntriesResponse.js
+++ node/AppendEntriesResponse.js
@@ -1,27 +1,40 @@
 import * as R from 'ramda';
 
+export const calculateMajorityMatchIndex = (leaderIndex, matchIndex) => {
+  const majorityThreshold = (Object.keys(matchIndex).length + 1) / 2;
+  const indices = Object.values(matchIndex);
+  for (let i = leaderIndex; i >= 0; i--) {
+    const count = R.filter(R.lte(i))(indices);
+    if (1 + count.length >= majorityThreshold) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 export default (node, event) => {
   if (event.success) {
+    const updatedMatchIndex = #{
+      ...node.volatileLeaderState.matchIndex,
+      [event.source]: event.request.prevLogIndex + event.request.entries.length
+    };
     return #[
       #{
-        type: 'SaveNodeVolatileLeaderState',
+        type: 'SaveVolatileLeaderState',
         source: 'leader',
         state: #{
           nextIndex: #{
             ...node.volatileLeaderState.nextIndex,
             [event.source]: event.request.prevLogIndex + event.request.entries.length + 1
           },
-          matchIndex: #{
-            ...node.volatileLeaderState.matchIndex,
-            [event.source]: event.request.prevLogIndex + event.request.entries.length
-          }
+          matchIndex: updatedMatchIndex
         }
       }
     ];
   } else {
     return #[
       #{
-        type: 'SaveNodeVolatileLeaderState',
+        type: 'SaveVolatileLeaderState',
         source: 'leader',
         state: #{
           ...node.volatileLeaderState,
--- node/AppendEntriesResponse.spec.js
+++ node/AppendEntriesResponse.spec.js
@@ -1,12 +1,81 @@
-import next from './AppendEntriesResponse'
+import next, {calculateMajorityMatchIndex} from './AppendEntriesResponse'
 
 test('leader updates nextIndex and matchIndex on successful AppendEntries', () => {
+  const peers = #['leader', 'follower', 'another follower', 'yet another follower'];
+  const configuration = #{
+    peers
+  };
+  const node = #{
+    id: 'leader',
+    volatileLeaderState: #{
+      nextIndex: #{
+        follower: 1,
+        'another follower': 1,
+        'yet another follower': 1
+      },
+      matchIndex: #{
+        follower: 0,
+        'another follower': 0,
+        'yet another follower': 0
+      }
+    }
+  };
+
+  const events = next(node, #{
+      type: 'AppendEntriesResponse',
+      source: 'follower',
+      destination: 'leader',
+      term: 1,
+      success: true,
+      request: #{
+        type: 'AppendEntriesRequest',
+        source: 'leader',
+        destination: 'follower',
+        term: 1,
+        leaderId: 'leader',
+        prevLogIndex: 0,
+        prevLogTerm: 1,
+        entries: #[
+          #{
+            term: 1,
+            command: 'do thing'
+          }
+        ],
+        leaderCommit: 0
+      }
+    })
+
+  expect(events).toEqual(#[
+    #{
+      type: 'SaveVolatileLeaderState',
+      source: 'leader',
+      state: #{
+        nextIndex: #{
+          follower: 2,
+          'another follower': 1,
+          'yet another follower': 1
+        },
+        matchIndex: #{
+          follower: 1,
+          'another follower': 0,
+          'yet another follower': 0
+        }
+      }
+    }
+  ]);
+});
+
+xtest('leader updates commitIndex when a log has be replicated to a majority of followers', () => {
   const peers = #['leader', 'follower', 'another follower'];
   const configuration = #{
     peers
   };
   const node = #{
     id: 'leader',
+    volatileState: #{
+      commitIndex: 0,
+      lastApplied: 0
+    },
     volatileLeaderState: #{
       nextIndex: #{
         follower: 1,
@@ -45,7 +114,7 @@ test('leader updates nextIndex and matchIndex on successful AppendEntries', () =
 
   expect(events).toEqual(#[
     #{
-      type: 'SaveNodeVolatileLeaderState',
+      type: 'SaveVolatileState',
       source: 'leader',
       state: #{
         nextIndex: #{
@@ -57,91 +126,24 @@ test('leader updates nextIndex and matchIndex on successful AppendEntries', () =
           'another follower': 0
         }
       }
+    },
+    #{
+      type: 'SaveVolatileState',
+      source: 'leader',
+      volatileState: #{
+        commitIndex: 1,
+        lastApplied: 0
+      }
     }
   ]);
 });
 
-// test('leader updates commitIndex when a log has be replicated to a majority of followers', () => {
-//   const peers = #['leader', 'follower', 'another follower'];
-//   const configuration = #{
-//     peers
-//   };
-//   const nodes = #[
-//     #{
-//       id: 'follower',
-//       configuration,
-//       state: #{
-//         currentTerm: 1,
-//         votedFor: 'leader',
-//         log: #[
-//           #{
-//             term: 1,
-//             command: ''
-//           },
-//           #{
-//             term: 1,
-//             command: 'do thing'
-//           }
-//         ]
-//       },
-//       volatileState: #{
-//         commitIndex: 0,
-//         lastApplied: 0
-//       }
-//     },
-//     #{
-//       id: 'leader',
-//       volatileLeaderState: #{
-//         nextIndex: #{
-//           follower: 1,
-//           'another follower': 1
-//         },
-//         matchIndex: #{
-//           follower: 0,
-//           'another follower': 0
-//         }
-//       }
-//     }
-//   ];
-
-//   const events = next(nodes, #{
-//       type: 'AppendEntriesResponse',
-//       source: 'follower',
-//       destination: 'leader',
-//       term: 1,
-//       success: true,
-//       request: #{
-//         type: 'AppendEntriesRequest',
-//         source: 'leader',
-//         destination: 'follower',
-//         term: 1,
-//         leaderId: 'leader',
-//         prevLogIndex: 0,
-//         prevLogTerm: 1,
-//         entries: #[
-//           #{
-//             term: 1,
-//             command: 'do thing'
-//           }
-//         ],
-//         leaderCommit: 0
-//       }
-//     })
-
-//   expect(events).toEqual(#[
-//     #{
-//       type: 'SaveNodeVolatileLeaderState',
-//       source: 'leader',
-//       state: #{
-//         nextIndex: #{
-//           follower: 2,
-//           'another follower': 1
-//         },
-//         matchIndex: #{
-//           follower: 1,
-//           'another follower': 0
-//         }
-//       }
-//     }
-//   ]);
-// });
+describe('calculateMajorityMatchIndex', () => {
+  test.each([
+    {leaderIndex: 1, matchIndex: #{f1: 0, f2: 0}, expectedMajority: 0},
+    {leaderIndex: 1, matchIndex: #{f1: 1, f2: 0}, expectedMajority: 1},
+    {leaderIndex: 3, matchIndex: #{f1: 3, f2: 2, f3: 1, f4: 1}, expectedMajority: 2}
+  ])('leaderIndex $leaderIndex & $matchIndex should have majority of $expectedMajority', ({leaderIndex, matchIndex, expectedMajority}) => {
+    expect(calculateMajorityMatchIndex(leaderIndex, matchIndex)).toBe(expectedMajority);
+  })
+})
