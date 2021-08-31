2997198867f41d8a68629b193f0002acfc7a5ef6
Ronald Chen <ronald@battlefy.com>
2021-08-31 11:02:52 -0700

    implemented client request

---
+++ node/ClientRequest.js
@@ -0,0 +1,36 @@
+import * as R from 'ramda';
+
+export default (node, event) => {
+  const entries = #[
+    ...event.commands.map((command) => #{
+      term: node.state.currentTerm,
+      command
+    })
+  ];
+  return #[
+    #{
+      type: 'SaveNodeState',
+      source: node.id,
+      state: #{
+        ...node.state,
+        log: #[
+          ...node.state.log,
+          ...entries
+        ]
+      }
+    },
+    ...R.pipe(
+      R.reject(R.equals(node.id)),
+      R.map((peer) => #{
+        type: 'AppendEntriesRequest',
+        destination: peer,
+        term: node.state.currentTerm,
+        leaderId: node.id,
+        prevLogIndex: node.state.log.length - 1,
+        prevLogTerm: node.state.log[node.state.log.length - 1].term,
+        entries,
+        leaderCommit: node.volatileState.commitIndex
+      })
+    )(node.configuration.peers)
+  ];
+};
---
+++ node/ClientRequest.spec.js
@@ -0,0 +1,94 @@
+import next from './ClientRequest';
+
+test('client request are distributed from leader to peers', () => {
+    const peers = #['leader', 'follower', 'another follower'];
+    const configuration = #{
+      peers
+    };
+    const node = #{
+      id: 'leader',
+      configuration,
+      state: #{
+        currentTerm: 1,
+        votedFor: 'leader',
+        log: #[
+          #{
+            term: 1,
+            command: ''
+          }
+        ]
+      },
+      volatileState: #{
+        commitIndex: 0,
+        lastApplied: 0
+      },
+      volatileLeaderState: #{
+        nextIndex: #{
+          follower: 1,
+          'another follower': 1
+        },
+        matchIndex: #{
+          follower: 0,
+          'another follower': 0
+        }
+      }
+    };
+
+    const events = next(node, #{
+      type: 'ClientRequest',
+      source: 'client',
+      destination: 'leader',
+      commands: #['do thing']
+    });
+
+    expect(events).toEqual(#[
+      #{
+        type: 'SaveNodeState',
+        source: 'leader',
+        state: #{
+          currentTerm: 1,
+          votedFor: 'leader',
+          log: #[
+            #{
+              term: 1,
+              command: ''
+            },
+            #{
+              term: 1,
+              command: 'do thing'
+            }
+          ]
+        }
+      },
+      #{
+        type: 'AppendEntriesRequest',
+        destination: 'follower',
+        term: 1,
+        leaderId: 'leader',
+        prevLogIndex: 0,
+        prevLogTerm: 1,
+        entries: #[
+          #{
+            term: 1,
+            command: 'do thing'
+          }
+        ],
+        leaderCommit: 0
+      },
+      #{
+        type: 'AppendEntriesRequest',
+        destination: 'another follower',
+        term: 1,
+        leaderId: 'leader',
+        prevLogIndex: 0,
+        prevLogTerm: 1,
+        entries: #[
+          #{
+            term: 1,
+            command: 'do thing'
+          }
+        ],
+        leaderCommit: 0
+      }
+    ]);
+  });
