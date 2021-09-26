15d97fe9c3f2266332384eea7fc0e8b5e827b376
Ronald Chen <ronald@battlefy.com>
2021-09-25 23:51:51 -0700

    turned naviation into a tab bar

--- worksheet.jsx
+++ worksheet.jsx
@@ -1,17 +1,54 @@
 import React, { useMemo } from 'react';
 import styled from "@emotion/styled";
 
-const Container = styled.div`
+const TabBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  padding-top: 10px;
+  position: relative;
+  height: 100%;
+`;
+
+const Tabs = styled.div`
   display: flex;
   flex-direction: row;
   gap: 5px;
 `;
 
-const Link = styled.a`
-  font-weight: ${({ active }) => active
-    ? 'bold'
-    : 'normal'
+const Tab = ({ active, href, children }) => {
+  return <TabContainer active={active}>
+    <a href={href}>{children}</a>
+  </TabContainer>;
+};
+
+const TabContainer = styled.div`
+  padding-left: 5px;
+  padding-right: 5px;
+  border-color: ${({ active }) => active ? 'black' : 'dimgrey'};
+  border-width: 2px;
+  border-style: solid;
+  border-bottom-color: ${({ active }) => active ? 'white' : 'black'};
+  border-radius: 8px 8px 0 0;
+  height: 20px;
+  z-index: 2;
+
+  a {
+    text-decoration: none;
+    color: ${({ active }) => active ? 'black' : 'dimgrey'};
   }
+`;
+
+const TabBody = styled.div`;
+  position: absolute;
+  top: 32px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-top-color: black;
+  border-top-style: solid;
+  border-top-width: 2px;
+  z-index: 1;
 `;
 
 const paths = [
@@ -24,22 +61,26 @@ const paths = [
     label: 'Events'
   },
 ];
+
 export const Navigation = ({ children }) => {
   const contextPath = useMemo(() => {
     const [, contextPath] = window.location.pathname.split('/');
     return `/${contextPath}`;
   }, [window.location.pathname]);
 
-  console.log({ contextPath });
   return <>
-    <Container>
+    <TabBar>
       <span>Worksheets:</span>
-      {paths.map(({ href, label }) => <Link
-        key={href}
-        href={href}
-        active={href === contextPath}
-      >{label}</Link>)}
-    </Container>
-    {children}
+      <Tabs>
+        {paths.map(({ href, label }) => <Tab
+          key={href}
+          href={href}
+          active={href === contextPath}
+        >{label}</Tab>)}
+      </Tabs>
+      <TabBody>
+        {children}
+      </TabBody>
+    </TabBar>
   </>;
 };
